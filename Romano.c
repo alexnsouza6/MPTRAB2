0a8c3dc1aa7c4c8056761b2b9e97a95f39f7ff68
Alex Nascimento Souza <alexnsouza6@hotmail.com>
2016-09-22 20:59:02 -0300

    Teste realizado para todas as unidades

--- Romano.c
+++ Romano.c
@@ -4,11 +4,30 @@
 #include "Romano.h"
 
 int transforma_romano(const char* num_romano){
-	int num_arabe=0, i=0;
+	int decimal=0, i=0;
+	int flag=0;
 
 	while(num_romano[i]){
-		if(num_romano[i]=='I') num_arabe+=1;
+		if(num_romano[i]=='I'){ 
+			if(flag==0) decimal++; //Se nao preceder nenhuma letra
+			if(num_romano[i+1]=='V') flag=1; //Se preceder V
+			else if(num_romano[i+1]=='X') flag=2; //Se preceder X
+		}
+
+		if(num_romano[i]=='V'){
+			if(flag==1) decimal+=3;
+			else decimal+=5;
+			if(num_romano[i+1]=='I') flag=0;
+		}
+
+		if(num_romano[i]=='X'){
+			if(flag==2) decimal+=8;
+			else if(num_romano[i+1]=='L') flag=3;
+			else if(num_romano[i+1]=='V') flag=2;
+			else decimal+=10;
+		}
+
 		i++;
 	}
-	return num_arabe;
+	return decimal;
 }
--- Teste_Romano.c
+++ Teste_Romano.c
@@ -3,12 +3,16 @@
 #include <stdlib.h>
 #include "Romano.h"
 
-TEST (TestaROMANO, Unit) {
+TEST(TestaROMANO, Unit){
 
-EXPECT_EQ (1, transforma_romano("I"));
-EXPECT_EQ (2, transforma_romano("II"));
-EXPECT_EQ (3, transforma_romano("III"));
-    
+	EXPECT_EQ (1, transforma_romano("I"));
+	EXPECT_EQ (2, transforma_romano("II"));
+	EXPECT_EQ (3, transforma_romano("III"));
+	EXPECT_EQ (4, transforma_romano("IV"));
+	EXPECT_EQ (5, transforma_romano("V"));
+	EXPECT_EQ (8, transforma_romano("VIII"));
+	EXPECT_EQ (9, transforma_romano("IX"));
+	EXPECT_EQ (10, transforma_romano("X"));	
 }
 
 int main(int argc, char **argv) {
