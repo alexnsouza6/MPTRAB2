670bc4d99e347b394c2d22e0c0090b1949e74772
Alex Nascimento Souza <alexnsouza6@hotmail.com>
2016-09-22 21:18:06 -0300

    Teste realizado para Dezenas (ate 40)

--- Romano.c
+++ Romano.c
@@ -7,6 +7,13 @@ int transforma_romano(const char* num_romano){
 	int decimal=0, i=0;
 	int flag=0;
 
+	if(num_romano[0]=='I') flag=0;
+	if(num_romano[0]=='V') flag=2;
+	if(num_romano[0]=='X') flag=3;
+	if(num_romano[0]=='L') flag=4;
+	if(num_romano[0]=='C') flag=5;
+
+
 	while(num_romano[i]){
 		if(num_romano[i]=='I'){ 
 			if(flag==0) decimal++; //Se nao preceder nenhuma letra
@@ -16,15 +23,16 @@ int transforma_romano(const char* num_romano){
 
 		if(num_romano[i]=='V'){
 			if(flag==1) decimal+=3;
-			else decimal+=5;
+			else if(flag==2) decimal+=5;
 			if(num_romano[i+1]=='I') flag=0;
 		}
 
 		if(num_romano[i]=='X'){
 			if(flag==2) decimal+=8;
-			else if(num_romano[i+1]=='L') flag=3;
-			else if(num_romano[i+1]=='V') flag=2;
-			else decimal+=10;
+			else if(flag==3) decimal+=10;
+			if(num_romano[i+1]=='I') flag=0;
+			if(num_romano[i+1]=='L') flag=3;
+			if(num_romano[i+1]=='V') flag=2;
 		}
 
 		i++;
--- Teste_Romano.c
+++ Teste_Romano.c
@@ -4,7 +4,6 @@
 #include "Romano.h"
 
 TEST(TestaROMANO, Unit){
-
 	EXPECT_EQ (1, transforma_romano("I"));
 	EXPECT_EQ (2, transforma_romano("II"));
 	EXPECT_EQ (3, transforma_romano("III"));
@@ -13,6 +12,19 @@ TEST(TestaROMANO, Unit){
 	EXPECT_EQ (8, transforma_romano("VIII"));
 	EXPECT_EQ (9, transforma_romano("IX"));
 	EXPECT_EQ (10, transforma_romano("X"));	
+}
+
+TEST(TestaROMANO, Tens){
+	EXPECT_EQ (11, transforma_romano("XI"));
+	EXPECT_EQ (18, transforma_romano("XVIII"));
+	EXPECT_EQ (23, transforma_romano("XXIII"));
+	EXPECT_EQ (28, transforma_romano("XXVIII"));
+	EXPECT_EQ (29, transforma_romano("XXIX"));
+	EXPECT_EQ (30, transforma_romano("XXX"));
+	EXPECT_EQ (35, transforma_romano("XXXV"));
+	EXPECT_EQ (39, transforma_romano("XXXIX"));	
+
+
 }
 
 int main(int argc, char **argv) {
