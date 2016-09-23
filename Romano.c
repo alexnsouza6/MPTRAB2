bf90bc3b767c9ca2716cf92ab124da7e1e495421
Alex Nascimento Souza <alexnsouza6@hotmail.com>
2016-09-23 15:26:45 -0300

    Finalização do código tão bem quanto os comentários

--- Romano.c
+++ Romano.c
@@ -1,18 +1,36 @@
+/**
+	\mainpage
+	\author Alex Nascimento Souza 15/0115474
+	\date Realease: 23/09/2016
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "Romano.h"
 
+/** \brief Função que converte o número romano em número árabe (decimal) */
 int transforma_romano(const char* num_romano){
+	
+	/**
+	\details A função recebe o string correspondente ao numero romano e realiza a conversão
+	\param checador é a flag responsável pela validação do numero romano inserido
+	\param decimal é o resultado da conversão
+	\param flag é responsável pela orientação quanto ao próximo algarismo. EX: num_romano[0]=='I', se num_romano[1]=='V, então flag=2
+	\return O número em arábico, resultado da conversão (variável decimal)
+	*/
+
 	int decimal=0, i=0;
 	int flag;
 	int checador;
-	char c[30];
-	strcpy(c,num_romano);
 	
-	checador=valida_num_romano(c);
 
-	if(checador==0){
+	checador=valida_num_romano(num_romano);
+
+	if(checador==0){ //Checador e responsavel por analisar se o numero romano e valido. Em  caso de 0, o processo de conversao e autorizado
+	
+	/*Verifica qual a primeira letra da string */
 	if(num_romano[0]=='I') flag=0;
 	if(num_romano[0]=='V') flag=2;
 	if(num_romano[0]=='X') flag=3;
@@ -20,6 +38,8 @@ int transforma_romano(const char* num_romano){
 	if(num_romano[0]=='C') flag=5;
 	if(num_romano[0]=='D') flag=6;
 	if(num_romano[0]=='M') flag=7;
+
+	/*Processo de conversao*/
 	while(num_romano[i]){
 		if(num_romano[i]=='I'){ 
 			if(flag==0) decimal++; //Se nao preceder nenhuma letra
@@ -28,58 +48,58 @@ int transforma_romano(const char* num_romano){
 		}
 
 		if(num_romano[i]=='V'){
-			if(flag==1) decimal+=3;
+			if(flag==1) decimal+=3; //Se V proceder um I
 			else if(flag==2) decimal+=5;
-			if(num_romano[i+1]=='I') flag=0;
+			if(num_romano[i+1]=='I') flag=0; //Se preceder I
 		}
 
 		if(num_romano[i]=='X'){
 			if(flag==3) decimal+=10;
-			else if(flag==2) decimal+=8;
-			if(num_romano[i+1]=='I') flag=0;
-			if(num_romano[i+1]=='L') flag=3;
-			if(num_romano[i+1]=='V') flag=2;
-			if(num_romano[i+1]=='C') flag=4;
+			else if(flag==2) decimal+=8; //Se X proceder um I
+			if(num_romano[i+1]=='I') flag=0; //Se preceder I
+			if(num_romano[i+1]=='L') flag=3; //Se preceder L
+			if(num_romano[i+1]=='V') flag=2; //Se preceder V
+			if(num_romano[i+1]=='C') flag=4; //Se preceder C
 		}
 
 		if(num_romano[i]=='L'){
 			if(flag==4) decimal+=50;
-			else if(flag==3) decimal+=30;
-			if(num_romano[i+1]=='X') flag=3;
-			if(num_romano[i+1]=='I') flag=0;
-			if(num_romano[i+1]=='V') flag=2;
+			else if(flag==3) decimal+=30; //Se L proceder um X 
+			if(num_romano[i+1]=='X') flag=3; //Se preceder X
+			if(num_romano[i+1]=='I') flag=0; //Se preceder I
+			if(num_romano[i+1]=='V') flag=2; //Se preceder V
 		}
 
 		if(num_romano[i]=='C'){
 			if(flag==5) decimal+=100;
-			else if(flag==4) decimal+=80;
-			if(num_romano[i+1]=='X') flag=3;
-			if(num_romano[i+1]=='I') flag=0;
-			if(num_romano[i+1]=='V') flag=2;
-			if(num_romano[i+1]=='L') flag=4;
-			if(num_romano[i+1]=='D') flag=5;
-			if(num_romano[i+1]=='M') flag=6;
+			else if(flag==4) decimal+=80; //Se C proceder um X
+			if(num_romano[i+1]=='X') flag=3; //Se preceder X
+			if(num_romano[i+1]=='I') flag=0; //Se preceder I
+			if(num_romano[i+1]=='V') flag=2; //Se preceder V
+			if(num_romano[i+1]=='L') flag=4; //Se preceder L
+			if(num_romano[i+1]=='D') flag=5; //Se preceder D
+			if(num_romano[i+1]=='M') flag=6; //Se preceder M
 		}
 
 		if(num_romano[i]=='D'){
 			if(flag==6) decimal+=500;
-			else if(flag==5) decimal+=300;
-			if(num_romano[i+1]=='X') flag=3;
-			if(num_romano[i+1]=='I') flag=0;
-			if(num_romano[i+1]=='V') flag=2;
-			if(num_romano[i+1]=='L') flag=4;
-			if(num_romano[i+1]=='C') flag=5;
+			else if(flag==5) decimal+=300; //Se D proceder um C
+			if(num_romano[i+1]=='X') flag=3; //Se preceder X
+			if(num_romano[i+1]=='I') flag=0; //Se preceder I
+			if(num_romano[i+1]=='V') flag=2; //Se preceder V
+			if(num_romano[i+1]=='L') flag=4; //Se preceder L
+			if(num_romano[i+1]=='C') flag=5; //Se preceder C
 		}
 
 		if(num_romano[i]=='M'){
 			if(flag==7) decimal+=1000;
-			else if(flag==6) decimal+=800;
-			if(num_romano[i+1]=='X') flag=3;
-			if(num_romano[i+1]=='I') flag=0;
-			if(num_romano[i+1]=='V') flag=2;
-			if(num_romano[i+1]=='L') flag=4;
-			if(num_romano[i+1]=='C') flag=5;
-			if(num_romano[i+1]=='D') flag=6;
+			else if(flag==6) decimal+=800; //Se M proceder um C
+			if(num_romano[i+1]=='X') flag=3; //Se preceder X
+			if(num_romano[i+1]=='I') flag=0; //Se preceder I
+			if(num_romano[i+1]=='V') flag=2; //Se preceder V
+			if(num_romano[i+1]=='L') flag=4; //Se preceder L
+			if(num_romano[i+1]=='C') flag=5; //Se preceder C
+			if(num_romano[i+1]=='D') flag=6; //Se preceder D
 		}
 
 		i++;
@@ -89,7 +109,17 @@ int transforma_romano(const char* num_romano){
 	else return -1;
 }
 
+/** \brief Função que verifica a validade do número romano inserido pelo usuário*/
 int valida_num_romano(const char* num_romano){
+
+	/**
+	\details A função recebe o string correspondente ao numero romano e realiza a verificação
+	\param contador Os contadores são responsáveis pela verificação do numero romano inserido
+	\return 0 caso o número em questão realmente exista ou -1 caso contrário.
+	*/
+
+
+
 	int i=0;
 	int contador=0;
 	int contadorc=0;
@@ -99,44 +129,48 @@ int valida_num_romano(const char* num_romano){
 
 		while(num_romano[i]){
 			if(num_romano[i]=='I'){
+				if(num_romano[i+1] == 'C') return -1; //Para caso a string seja "IC"
+				if(num_romano[i+1] == 'L') return -1; //Para caso a string seja "IL"
+				if(num_romano[i+1] == 'D') return -1; //Para caso a string seja "ID"
+				if(num_romano[i+1] == 'M') return -1; //Para caso a string seja "IM"
 				if(num_romano[i]==num_romano[i+1]) contador++;
-				if(contador==3) return -1;
+				if(contador==3) return -1; //Para caso a string seja "IIII" ou variacoes, como "VIIII" para representar 9.
 			}
 
 			if(num_romano[i]=='V'){
-				if(num_romano[i+1] == 'V') return -1;
-				if(num_romano[i+1] == 'D') return -1;
-				if(num_romano[i+1] == 'C') return -1;
-				if(num_romano[i+1] == 'L') return -1;
-				if(num_romano[i+1] == 'M') return -1;
-				if(num_romano[i+1] == 'X') return -1;
+				if(num_romano[i+1] == 'V') return -1; //Para caso a string seja "VV"
+				if(num_romano[i+1] == 'D') return -1; //Para caso a string seja "VD"
+				if(num_romano[i+1] == 'C') return -1; //Para caso a string seja "VC"
+				if(num_romano[i+1] == 'L') return -1; //Para caso a string seja "VL"
+				if(num_romano[i+1] == 'M') return -1; //Para caso a string seja "VM"
+				if(num_romano[i+1] == 'X') return -1; //Para caso a string seja "VX"
 			}
 
 			if(num_romano[i]=='X'){
-				if(num_romano[i]==num_romano[i+1]) contadorx++;
-				if(contadorx==3) return -1;
-				if(num_romano[i+1] == 'M') return -1;
-				if(num_romano[i+1] == 'D') return -1;
+				if(num_romano[i]==num_romano[i+1]) contadorx++; 
+				if(contadorx==3) return -1; //Para caso a string seja "XXXX" ou variacoes, como "XXXXI" para representar 41.
+				if(num_romano[i+1] == 'M') return -1; //Para caso a string seja "XM"
+				if(num_romano[i+1] == 'D') return -1; //Para caso a string seja "XD"
 			}
 
 			if(num_romano[i]=='L'){
-				if(num_romano[i+1] == 'M') return -1;
-				if(num_romano[i+1] == 'D') return -1;
-				if(num_romano[i+1] == 'L') return -1;		
+				if(num_romano[i+1] == 'M') return -1; //Para caso a string seja "LM"
+				if(num_romano[i+1] == 'D') return -1; //Para caso a string seja "LD"
+				if(num_romano[i+1] == 'L') return -1; //Para caso a string seja "LL"		
 			}
 
-			if(num_romano[i]=='C'){
+			if(num_romano[i]=='C'){ 
 				if(num_romano[i]==num_romano[i+1]) contadorc++;
-				if(contadorc==3) return -1;
+				if(contadorc==3) return -1; //Para caso a string seja "CCCC" ou variacoes, como "CCCCI" para representar 401.
 			}
 		
 
 			if(num_romano[i]=='D')
-				if(num_romano[i+1] == 'M') return -1;
+				if(num_romano[i+1] == 'M') return -1; //Para caso a string seja "DM"
 
 			if(num_romano[i]=='M'){
 				if(num_romano[i]==num_romano[i+1]) contadorm++;
-				if(contadorm==3) return -1;
+				if(contadorm==3) return -1; //Para caso a string seja "MMMMM" ou variacoes, como "MMMMM" para representar 401.
 			}
 			i++;
 		}
--- Teste_Romano.c
+++ Teste_Romano.c
@@ -3,79 +3,91 @@
 #include <stdlib.h>
 #include "Romano.h"
 
+/** \brief Teste realizado para os números entre 1 e 10*/
 TEST(TestaROMANO, Units){
-	EXPECT_EQ (1, transforma_romano("I"));
-	EXPECT_EQ (2, transforma_romano("II"));
-	EXPECT_EQ (3, transforma_romano("III"));
-	EXPECT_EQ (4, transforma_romano("IV"));
-	EXPECT_EQ (5, transforma_romano("V"));
-	EXPECT_EQ (8, transforma_romano("VIII"));
-	EXPECT_EQ (9, transforma_romano("IX"));
-	EXPECT_EQ (10, transforma_romano("X"));	
+	EXPECT_EQ (1, transforma_romano("I")); //Dado a entrada "I", espera-se obter o retorno "1"
+	EXPECT_EQ (2, transforma_romano("II")); //Dado a entrada "II", espera-se obter o retorno "2"
+	EXPECT_EQ (3, transforma_romano("III")); //Dado a entrada "III", espera-se obter o retorno "3"
+	EXPECT_EQ (4, transforma_romano("IV")); //Dado a entrada "IV", espera-se obter o retorno "4"
+	EXPECT_EQ (5, transforma_romano("V")); //Dado a entrada "V", espera-se obter o retorno "5"
+	EXPECT_EQ (8, transforma_romano("VIII")); //Dado a entrada "VIII", espera-se obter o retorno "8"
+	EXPECT_EQ (9, transforma_romano("IX")); //Dado a entrada "IX", espera-se obter o retorno "9"
+	EXPECT_EQ (10, transforma_romano("X"));	 //Dado a entrada "X", espera-se obter o retorno "10"
 }
 
+
+/** \brief Teste realizado para alguns números entre 11 e 39*/
 TEST(TestaROMANO, Tens){
-	EXPECT_EQ (11, transforma_romano("XI"));
-	EXPECT_EQ (18, transforma_romano("XVIII"));
-	EXPECT_EQ (23, transforma_romano("XXIII"));
-	EXPECT_EQ (28, transforma_romano("XXVIII"));
-	EXPECT_EQ (29, transforma_romano("XXIX"));
-	EXPECT_EQ (30, transforma_romano("XXX"));
-	EXPECT_EQ (35, transforma_romano("XXXV"));
-	EXPECT_EQ (39, transforma_romano("XXXIX"));	
+	EXPECT_EQ (11, transforma_romano("XI")); //Dado a entrada "XI", espera-se obter o retorno "10"
+	EXPECT_EQ (18, transforma_romano("XVIII")); //Dado a entrada "XVIII", espera-se obter o retorno "18"
+	EXPECT_EQ (23, transforma_romano("XXIII")); //Dado a entrada "XXIII", espera-se obter o retorno "23"
+	EXPECT_EQ (28, transforma_romano("XXVIII")); //Dado a entrada "XXVIII", espera-se obter o retorno "28"
+	EXPECT_EQ (29, transforma_romano("XXIX")); //Dado a entrada "XXIX", espera-se obter o retorno "29"
+	EXPECT_EQ (30, transforma_romano("XXX")); //Dado a entrada "XXX", espera-se obter o retorno "30"
+	EXPECT_EQ (35, transforma_romano("XXXV")); //Dado a entrada "XXXV", espera-se obter o retorno "35"
+	EXPECT_EQ (39, transforma_romano("XXXIX"));	 //Dado a entrada "XXXIX", espera-se obter o retorno "39"
 }
 
+/** \brief Teste realizado para alguns números entre 40 e 89*/
 TEST(TestaROMANO, Tens2){
-	EXPECT_EQ (40, transforma_romano("XL"));
-	EXPECT_EQ (50, transforma_romano("L"));
-	EXPECT_EQ (58, transforma_romano("LVIII"));
-	EXPECT_EQ (69, transforma_romano("LXIX"));
-	EXPECT_EQ (75, transforma_romano("LXXV"));
-	EXPECT_EQ (81, transforma_romano("LXXXI"));
-	EXPECT_EQ (87, transforma_romano("LXXXVII"));
+	EXPECT_EQ (40, transforma_romano("XL")); //Dado a entrada "XL", espera-se obter o retorno "40"
+	EXPECT_EQ (50, transforma_romano("L")); //Dado a entrada "L", espera-se obter o retorno "50"
+	EXPECT_EQ (58, transforma_romano("LVIII")); //Dado a entrada "LVIII", espera-se obter o retorno "58"
+	EXPECT_EQ (69, transforma_romano("LXIX")); //Dado a entrada "LXIX", espera-se obter o retorno "69"
+	EXPECT_EQ (75, transforma_romano("LXXV")); //Dado a entrada "LXXV", espera-se obter o retorno "75"
+	EXPECT_EQ (81, transforma_romano("LXXXI")); //Dado a entrada "LXXXI", espera-se obter o retorno "81"
+	EXPECT_EQ (87, transforma_romano("LXXXVII")); //Dado a entrada "LXXXVII", espera-se obter o retorno "87"
 }
 
+/** \brief Teste realizado para alguns números entre 90 e 390*/
 TEST(TestaROMANO, Hundreds){
-	EXPECT_EQ (90, transforma_romano("XC"));
-	EXPECT_EQ (100, transforma_romano("C"));
-	EXPECT_EQ (109, transforma_romano("CIX"));
-	EXPECT_EQ (157, transforma_romano("CLVII"));
-	EXPECT_EQ (200, transforma_romano("CC"));
-	EXPECT_EQ (240, transforma_romano("CCXL"));
-	EXPECT_EQ (390, transforma_romano("CCCXC"));
+	EXPECT_EQ (90, transforma_romano("XC")); //Dado a entrada "XC", espera-se obter o retorno "90"
+	EXPECT_EQ (100, transforma_romano("C")); //Dado a entrada "C", espera-se obter o retorno "100"
+	EXPECT_EQ (109, transforma_romano("CIX")); //Dado a entrada "CIX", espera-se obter o retorno "109"
+	EXPECT_EQ (157, transforma_romano("CLVII")); //Dado a entrada "CLVII", espera-se obter o retorno "157"
+	EXPECT_EQ (200, transforma_romano("CC")); //Dado a entrada "CC", espera-se obter o retorno "200"
+	EXPECT_EQ (240, transforma_romano("CCXL")); //Dado a entrada "CCXL", espera-se obter o retorno "240"
+	EXPECT_EQ (390, transforma_romano("CCCXC")); //Dado a entrada "CCCXC", espera-se obter o retorno "390"
 }
 
+/** \brief Teste realizado para alguns números entre 400 e 899*/
 TEST(TestaROMANO, Hundreds2){
-	EXPECT_EQ (400, transforma_romano("CD"));
-	EXPECT_EQ (500, transforma_romano("D"));
-	EXPECT_EQ (572, transforma_romano("DLXXII"));
-	EXPECT_EQ (669, transforma_romano("DCLXIX"));
-	EXPECT_EQ (882, transforma_romano("DCCCLXXXII"));
-	EXPECT_EQ (899, transforma_romano("DCCCXCIX"));
+	EXPECT_EQ (400, transforma_romano("CD")); //Dado a entrada "CD", espera-se obter o retorno "400"
+	EXPECT_EQ (500, transforma_romano("D")); //Dado a entrada "D", espera-se obter o retorno "500"
+	EXPECT_EQ (572, transforma_romano("DLXXII")); //Dado a entrada "DLXXII", espera-se obter o retorno "572"
+	EXPECT_EQ (669, transforma_romano("DCLXIX")); //Dado a entrada "DCLXIX", espera-se obter o retorno "669"
+	EXPECT_EQ (882, transforma_romano("DCCCLXXXII")); //Dado a entrada "DCCCLXXXII", espera-se obter o retorno "882"
+	EXPECT_EQ (899, transforma_romano("DCCCXCIX")); //Dado a entrada "DCCCXCIX", espera-se obter o retorno "899"
 }
 
+/** \brief Teste realizado para alguns números entre 900 e 3000*/
 TEST(TestaROMANO, Thousands){
-	EXPECT_EQ (900, transforma_romano("CM"));
-	EXPECT_EQ (1032, transforma_romano("MXXXII"));
-	EXPECT_EQ (1500, transforma_romano("MD"));
-	EXPECT_EQ (1869, transforma_romano("MDCCCLXIX"));
-	EXPECT_EQ (2049, transforma_romano("MMXLIX"));
-	EXPECT_EQ (2204, transforma_romano("MMCCIV"));
+	EXPECT_EQ (900, transforma_romano("CM")); //Dado a entrada "CM", espera-se obter o retorno "900"
+	EXPECT_EQ (1032, transforma_romano("MXXXII")); //Dado a entrada "MXXXII", espera-se obter o retorno "1032"
+	EXPECT_EQ (1500, transforma_romano("MD")); //Dado a entrada "MD", espera-se obter o retorno "1500"
+	EXPECT_EQ (1869, transforma_romano("MDCCCLXIX")); //Dado a entrada "MDCCCLXIX", espera-se obter o retorno "1869"
+	EXPECT_EQ (2049, transforma_romano("MMXLIX")); //Dado a entrada "MMXLIX", espera-se obter o retorno "2049"
+	EXPECT_EQ (2204, transforma_romano("MMCCIV")); //Dado a entrada "MMCCIV", espera-se obter o retorno "2204"
 }
 
+/** \brief Teste realizado para números inválidos*/
 TEST(TestaROMANO, Invalid_Operands){
-	ASSERT_EQ (-1, transforma_romano("IIII"));
-	EXPECT_EQ (-1, transforma_romano("VX"));
-	EXPECT_EQ (-1, transforma_romano("VL"));
-	EXPECT_EQ (-1, transforma_romano("VC"));
-	EXPECT_EQ (-1, transforma_romano("VVVV"));
-	EXPECT_EQ (-1, transforma_romano("XD"));
-	EXPECT_EQ (-1, transforma_romano("XXXX"));
-	EXPECT_EQ (-1, transforma_romano("LD"));
-	EXPECT_EQ (-1, transforma_romano("LL"));
-	EXPECT_EQ (-1, transforma_romano("DM"));
-	EXPECT_EQ (-1, transforma_romano("CCCC"));
-	EXPECT_EQ (-1, transforma_romano("MMMM"));	
+	ASSERT_EQ (-1, transforma_romano("IIII")); //Dado a entrada "IIII", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("IC")); //Dado a entrada "IC", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("IL")); //Dado a entrada "IL", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("ID")); //Dado a entrada "ID", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("IM")); //Dado a entrada "IM", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("VX")); //Dado a entrada "VX", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("VL")); //Dado a entrada "VL", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("VC")); //Dado a entrada "VC", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("VVVV")); //Dado a entrada "VVVV", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("XD")); //Dado a entrada "XD", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("XXXX")); //Dado a entrada "XXXX", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("LD")); //Dado a entrada "LD", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("LL")); //Dado a entrada "LL", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("DM")); //Dado a entrada "DM", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("CCCC")); //Dado a entrada "CCCC", espera-se obter o retorno "-1"
+	EXPECT_EQ (-1, transforma_romano("MMMM"));	 //Dado a entrada "MMMM", espera-se obter o retorno "-1"
 }
 
 
